bf7e2732d62c32c43de2d9549d1cf29e5df51689
SpaceIIl <hrujan@outlook.com>
2023-01-22 11:37:53 +0100

    master

--- frontend/pages/comp/NavBuy.tsx
+++ frontend/pages/comp/NavBuy.tsx
@@ -3,11 +3,11 @@ import Link from 'next/link'
 
 const NavBuy: NextPage = () => {
     return (
-      <div className=' text-white float-right'>
-        <ul className=''>
-            <li><Link className='bg-black float-right' href="/howToBuy"><p className='my-3 mx-6'>Jak Nakoupit</p></Link></li><br />
-            <li><Link className='bg-black float-right' href="/howToBuy"><p className='my-3 mx-6'>Jak Nakoupit</p></Link></li><br />
-            <li><Link className='bg-black float-right' href="/howToBuy"><p className='my-3 mx-6'>Jak Nakoupit</p></Link></li><br />
+      <div className=' text-white'>
+        <ul className='absolute bg-black'>
+            <li><Link className='bg-black' href="/howToBuy"><p className='my-3 mx-6'>Jak Nakoupit</p></Link></li><br />
+            <li><Link className='bg-black' href="/howToBuy"><p className='my-3 mx-6'>Jak Nakoupit</p></Link></li><br />
+            <li><Link className='bg-black' href="/howToBuy"><p className='my-3 mx-6'>Jak Nakoupit</p></Link></li><br />
         </ul>
       </div>
     )
